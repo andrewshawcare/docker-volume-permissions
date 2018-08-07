020e7b76d17455936a9427626f65420feddd84fb
Andrew Shaw Care <andrewshawcare@gmail.com>
2018-08-07 22:49:30 +0000

    Switch to dynamic user/group creation instead of modification

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM ubuntu
 
-RUN apt-get update && apt-get install -y git && apt-get clean
-
-RUN groupadd --gid 1001 user
-RUN useradd --uid 1001 --gid 1001 user
-
 RUN mkdir /tmp/volume && touch /tmp/volume/build-file
 VOLUME ["/tmp/volume"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -e
-
-# Change UID and GID based off $HOST_UID and $HOST_GID
-groupmod --gid $HOST_GID user
-usermod --uid $HOST_UID --gid $HOST_GID user
-
-# Change ownership of all files and folders that used previous UID and GID
-find / -gid $HOST_GID -uid $HOST_UID | xargs chown "$HOST_UID:$HOST_GID"
+groupadd --gid $HOST_GID user
+useradd --uid $HOST_UID --gid $HOST_GID user
 
 su -c 'touch /tmp/volume/run-file' - user
 ls -al /tmp /tmp/volume
