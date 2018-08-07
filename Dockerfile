df236ec14b599366dde15837d496061a063a8d7f
Andrew Shaw Care <andrewshawcare@gmail.com>
2018-08-07 21:32:29 +0000

    Add initial solution

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM ubuntu
 
 RUN apt-get update && apt-get install -y git && apt-get clean
 
-RUN groupadd --gid 1000 user
-RUN useradd --uid 1000 --gid 1000 user
-USER user
+RUN groupadd --gid 1001 user
+RUN useradd --uid 1001 --gid 1001 user
 
 RUN mkdir /tmp/volume && touch /tmp/volume/build-file
 VOLUME ["/tmp/volume"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 # Change UID and GID based off $HOST_UID and $HOST_GID
+groupmod --gid $HOST_GID user
+usermod --uid $HOST_UID --gid $HOST_GID user
 
 # Change ownership of all files and folders that used previous UID and GID
-# find / -gid -uid | xargs chown "$HOST_UID:$HOST_GID"
+find / -gid $HOST_GID -uid $HOST_UID | xargs chown "$HOST_UID:$HOST_GID"
 
-touch /tmp/volume/run-file
+su -c 'touch /tmp/volume/run-file' - user
 ls -al /tmp /tmp/volume
